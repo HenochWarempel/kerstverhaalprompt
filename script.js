6142d882f8664c6d6de3e63ef4ff79d17ab6d360
HenochWarempel <143548932+HenochWarempel@users.noreply.github.com>
2023-11-07 14:49:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const collega = document.getElementById("collega").value;
         const beroepcollega = document.getElementById("beroepcollega").value;
         const genre = document.getElementById("genre").value;
-        const hobby = document.getElementById("hobby").value;
 
-        const tekst = `Schrijf een boeiend en rond kerstverhaal over ${naam}, die werkt als ${beroep} bij het bedrijf ${bedrijfsnaam}. Verwerk ook een bijrol voor ${collega}, die werkt als ${beroepcollega}. Benoem het bedrijf eenmalig, niet vaker. Schrijf het verhaal in het genre ${genre} en zorg voor een bevredigend einde.`;
+        const tekst = `Schrijf een boeiend en rond kerstverhaal over ${naam}, die werkt als ${beroep} bij het bedrijf ${bedrijfsnaam}. Benoem het bedrijf eenmalig, niet vaker. Verwerk ook een bijrol voor ${collega}, die werkt als ${beroepcollega}.  Het is belangrijk dat het verhaal geschreven is in het genre ${genre} en zorg voor een bevredigend einde.`;
         
         lopendeTekst.textContent = tekst;
     });
